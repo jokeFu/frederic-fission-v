53f2df81cbde7fb2e7d1aa432e466581b54b132f
Administrator <13555768695@163.com>
2020-02-14 10:05:30 +0800

    简单更改

--- src/module/cms/router/index.js
+++ src/module/cms/router/index.js
@@ -1,19 +1,20 @@
 import Home from '@/module/home/page/home.vue';
 import page_list from '@/module/cms/page/page_list.vue';
+
 export default [{
-    path: '/',
-    component: Home,
-    name: 'CMS',
-    hidden: false,
-    children:[
-      { path: '/cms/page/list',name:'页面列表', component: page_list,hidden:false }
+  path: '/',
+  component: Home,
+  name: 'CMS',
+  hidden: false,
+  children: [
+    {path: '/cms/page/list', name: '页面列表', component: page_list, hidden: false}
   ]
-    // ,
-    // redirect: '/home',
-    // children: [
-    //   {path: 'home', component: Home}
-    // ]
-  }/*,
+  // ,
+  // redirect: '/home',
+  // children: [
+  //   {path: 'home', component: Home}
+  // ]
+}/*,
   {
     path: '/login',
     component: Login,
